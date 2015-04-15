869bef74890a4a74918e9675136978fbfbc9a321
Hazius <rwr_pavel@mail.ru>
2015-04-16 00:10:36 +0300

    modify Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,12 +3,15 @@ require 'parser'
 class Page < ActiveRecord::Base
   attr_accessor :parsed_title
   validates :url, presence: true
+
   def self.parse_title!
     all.each{ |page| page.parse_title! }
   end
+
   def parse_title!
     @parsed_title = Parser.title_for(self.url).to_s
     @parsed_title.slice! "<title>"
     @parsed_title.slice! "</title>"
   end
+
 end
